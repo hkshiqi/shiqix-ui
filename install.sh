92a3a201cfea5cb4d0238a5981307e8a53ab4e17
hkshiqi <100076931+hkshiqi@users.noreply.github.com>
2022-04-25 02:07:06 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+function install_bibei
+{
+apt-get -y install wget
+echo 500000 >/proc/sys/kernel/pid_max
+echo 500000 >/proc/sys/kernel/threads-max
+echo 500000 >/proc/sys/vm/max_map_count
+echo 500000 >/proc/sys/fs/file-max
+ulimit -SHn 10240
+ulimit -SHs unlimited
+modprobe ip_conntrack
+echo 1000000 >/proc/sys/net/nf_conntrack_max
+lsmod |grep conntrack
+}
+
+function install_nginx_ubuntu
+{
+echo "安装nginx------"
+apt-get install epel-release -y
+apt-get update -y -y
+apt-get install -y nginx
+apt-get install nginx-mod-stream
+wget --no-check-certificate -O /etc/nginx/nginx.conf https://raw.githubusercontent.com/hkshiqi/Azure/main/nginx/nginx.conf
+chmod 777 /etc/nginx/nginx.conf
+mkdir /etc/nginx/zf.d
+chmod 777 /etc/nginx/zf.d
+wget --no-check-certificate -O /etc/nginx/zf.d/config.conf https://raw.githubusercontent.com/hkshiqi/Azure/main/nginx/zf.d/config.conf
+chmod 777 /etc/nginx/zf.d/config.conf
+systemctl restart nginx
+}
+
+function install_x-ui
+{
+echo "安装x-ui"
+bash <(curl -Ls https://raw.githubusercontent.com/hkshiqi/shiqix-ui/main/x-ui.sh)
+echo "开始下载必要文件"
+apt-get install unzip
+wget --no-check-certificate -O /root/azure.zip https://github.com/hkshiqi/Azure/archive/refs/heads/main.zip
+unzip /root/azure.zip
+chmod 777 /root/Azure-main/sk.sh
+mkdir /etc/Shadowsocks
+mv -f /root/Azure-main/Shadowsocks/sk5/* /etc/Shadowsocks/
+chmod 777 /etc/Shadowsocks
+cd /root/Azure-main
+echo "启动x-ui出口"
+./sk.sh
+rm -rf /root/Azure-main
+rm -rf /root/azure.zip
+}
